34e89160bd93bcdad18a5421a3998949b4f87ac9
kineticbeast69 <tiarishubham@gmail.com>
2026-04-06 14:57:36 +0530

    loading card ui updated

--- client/src/components/loading/LoadingCard.jsx
+++ client/src/components/loading/LoadingCard.jsx
@@ -1,7 +1,7 @@
 export default function LoadingCards() {
   return (
     // this is the ghost loading of blog cards
-    <aside className="w-full flex justify-center my-10 gap-7 sm:gap-5 ">
+    <aside className="w-full flex justify-center space-x-7 my-10  sm:gap-5 ">
       {/* card 1 */}
       <div className="flex flex-col bg-neutral-300 w-80 h-96 animate-pulse rounded-xl p-4 gap-4">
         <div className="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
@@ -23,6 +23,16 @@ export default function LoadingCards() {
         </div>
       </div>
       {/* card 3 */}
+      <div className="flex flex-col bg-neutral-300 w-80 h-96 animate-pulse rounded-xl p-4 gap-4">
+        <div className="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+        </div>
+      </div>
+      {/* card 4 */}
       <div className="flex flex-col bg-neutral-300 w-80 h-96 animate-pulse rounded-xl p-4 gap-4">
         <div className="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
         <div className="flex flex-col gap-2">
